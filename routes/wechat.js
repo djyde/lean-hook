6f912995b9614b22e303edb7d40998b9b9b96793
Randy <randypriv@gmail.com>
2016-02-16 00:35:57 +0800

    fix config

--- routes/wechat.js
+++ routes/wechat.js
@@ -2,10 +2,24 @@ var router = require('express').Router();
 var wxAPI = require('wechat-api')
 var config = require('../_config')
 
-var wx = new wxAPI(config.wechat.appId, config.wechat.appSecret)
+var path = require('path')
+var fs = require('fs')
+
+var wx = new wxAPI(config.wechat.appId, config.wechat.appSecret, function (callback) {
+  // 传入一个获取全局token的方法
+  fs.readFile(path.resolve(__dirname, '../', 'access_token.txt'), 'utf8', function (err, txt) {
+    if (err) {return callback(err);}
+    callback(null, JSON.parse(txt));
+  });
+}, function (token, callback) {
+  // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+  // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+  fs.writeFile(path.resolve(__dirname, '../', 'access_token.txt'), JSON.stringify(token), callback);
+})
+
 router
   .post('/textMessage', function(req, res) {
-    wx.sendText(config.openid, req.body.msg, function(err, result){
+    wx.sendText(config.wechat.openid, req.body.msg, function(err, result){
       if (err) {
         res.send(err)
       } else {
@@ -30,7 +44,7 @@ router
         break
     }
 
-    wx.sendText(config.openid, msg, function(err, result){
+    wx.sendText(config.wechat.openid, msg, function(err, result){
       if (err) {
         res.send(err)
       } else {
